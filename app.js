e4f49dd1fa9442c9d8d687b8a5ff7a748d7482b2
unknown <aishsurwase739@gmail.com>
2020-12-19 14:11:51 +0530

    added ejs template engine

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +17,7 @@ app.use(shopRoutes)
 
 app.use((req, res, next) => {
     // res.sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
-    res.render('page-not-found', { docTitle: 'Page not found' });
+    res.render('page-not-found', { docTitle: 'Page not found', path: '/page-not-found' });
 });
 
 app.listen(3000);
